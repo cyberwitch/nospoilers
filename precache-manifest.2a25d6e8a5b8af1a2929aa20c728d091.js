5b4d132e4acebe0a27e8933f680fd95b9b0d50ad
Danielle Weisz <dani@cyberwit.ch>
2019-12-04 21:23:04 -0800

    Updates

---
+++ precache-manifest.2a25d6e8a5b8af1a2929aa20c728d091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ff147caaf038edfb0b71713c46cb7a",
+    "url": "/nospoilers/index.html"
+  },
+  {
+    "revision": "59da971175117ce54607",
+    "url": "/nospoilers/static/css/2.693e7f7a.chunk.css"
+  },
+  {
+    "revision": "36de78bfc40dcc067ab9",
+    "url": "/nospoilers/static/css/main.918562b8.chunk.css"
+  },
+  {
+    "revision": "59da971175117ce54607",
+    "url": "/nospoilers/static/js/2.23f24c81.chunk.js"
+  },
+  {
+    "revision": "36de78bfc40dcc067ab9",
+    "url": "/nospoilers/static/js/main.4cbf62fd.chunk.js"
+  },
+  {
+    "revision": "a6498d52a325730e4f7e",
+    "url": "/nospoilers/static/js/runtime-main.77d7a79e.js"
+  }
+]);
